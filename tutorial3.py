1abd831ff6a2f380e0a0c413d0cd3150980b9ebf
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-21 11:51:42 -0400

    Updated from Brython Server: 9/21/2018 11:51:42 AM

--- tutorial3.py
+++ tutorial3.py
@@ -6,21 +6,18 @@ from ggame import LineStyle, Color, Sprite, Sound
 
 myapp = App()
 
+
+
 # A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
-ball = Sprite(ball_asset, (0, 0))
+ball = Sprite(ball_asset, (1, 1))
 # Original image is too big. Scale it to 1/10 its original size
 ball.scale = 0.1
 # custom attributes
 ball.direction = 1
 ball.go = True
 
-# define colors and line style
-green = Color(0x00ff00, 1)
-black = Color(0, 1)
-noline = LineStyle(0, black)
-# a rectangle asset and sprite to use as background
-bg_asset = RectangleAsset(myapp.width, myapp.height, noline, green)
-bg = Sprite(bg_asset, (0,0))
-
+# reverse - change the ball direction
+def reverse(b):
+    b.direction *= -1
 myapp.run()
